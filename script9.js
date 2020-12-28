55dce17fb09be627713c8b10852f192b39a68933
Luhivskyi <luhivskyi.d@ukr.net>
2020-12-28 17:10:28 +0300

    Цикл for...in 10
    Перебери объект apartment используя цикл for...in и запиши в массив keys все его ключи, а в массив values все значения его свойств.

---
+++ script9.js
@@ -0,0 +1,11 @@
+const apartment = {
+  descr: 'Просторная квартира в центре',
+  rating: 4,
+  price: 2153,
+};
+const keys = ['descr', 'rating', 'price'];
+const values = ['Просторная квартира в центре', 4, 2153];
+// Пиши код ниже этой строки
+for(key in keys){
+console.log(key)
+};
