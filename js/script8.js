20bc80a44c5b7218530603fb48b319b68bb072b4
Luhivskyi <luhivskyi.d@ukr.net>
2020-12-28 14:58:49 +0300

    Вычисляемые свойства 9
    Дополни код объявления объекта credentials так, чтобы в результате у него были два свойства: email и password, имена которых хранятся в переменных emailInputName и passwordInputName.
    
    Значением свойства email должна быть строка 'henry.carter@aptmail.com', а значением свойства password - строка 'jqueryismyjam'.

---
+++ js/script8.js
@@ -0,0 +1,9 @@
+const emailInputName = 'email';
+const passwordInputName = 'password';
+
+const credentials = {
+  // Пиши код ниже этой строки
+ [emailInputName] : 'henry.carter@aptmail.com',
+ [passwordInputName] : 'jqueryismyjam',  
+  
+};
