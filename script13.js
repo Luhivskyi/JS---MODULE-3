a311c25582046c1bbaadb4b62f7d073c730bc8ce
Luhivskyi <luhivskyi.d@ukr.net>
2020-12-29 14:45:23 +0300

    Подсчёт свойств 2.0    14
    Выполни рефакторинг функции countProps(object) используя метод Object.keys() и, возможно, цикл for...of.

---
+++ script13.js
@@ -0,0 +1,9 @@
+function countProps(object) {
+  // Пиши код ниже этой строки
+  let propCount = 0;
+const keys = Object.keys(object);
+   
+ return keys.length;
+  // Пиши код выше этой строки
+}
+ 
