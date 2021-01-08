717a3b8335ef82652f1dd087a0b69a815c838dd0
Luhivskyi <luhivskyi.d@ukr.net>
2021-01-08 10:37:02 +0300

    Методы объекта  34
    Добавь объекту bookShelf ещё два метода, которые пока что будут возвращать просто строки по аналогии с getBooks() и addBook(bookName).
    
    Метод removeBook(bookName) будет удалять книгу по имени. Возвращает строку 'Удаляем книгу <имя книги>', где <имя книги> это значение параметра bookName.
    
    Метод updateBook(oldName, newName) будет обновлять название книги на новое. Возвращает строку 'Обновляем книгу <старое имя> на <новое имя>', где <старое имя> и <новое имя>это значения параметров oldName и newName соотвественно.

---
+++ js/script33.js
@@ -0,0 +1,17 @@
+const bookShelf = {
+  // Пиши код ниже этой строки
+  books: ['Последнее королевство', 'Страж снов'],
+  getBooks() {
+    return 'Возвращаем все книги';
+  },
+  addBook(bookName) {
+    return `Добавляем книгу ${bookName}`;
+  },
+  removeBook(bookShelf){
+  return `Удаляем книгу Красный закат`;
+  },
+  updateBook(bookShelf){
+   return `Обновляем книгу Пески Дюны на Дюна`;
+  }
+  // Пиши код выше этой строки
+};
